927d1692065d0d9fb2a424bf6844f79132847ae0
Boris Isac <59101149+SPAWNKID19XX@users.noreply.github.com>
2025-10-02 17:06:45 +0100

    Delete server directory

--- server/users/permitions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsSuperuserOrCreate(BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'POST':
-            return True
-        return bool(request.user and request.user.is_authenticated and request.user.is_superuser)
