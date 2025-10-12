ba67ad25c5efff26e6c002457781dd67573da237
boris-isac <borisisac@gmail.com>
2025-10-12 19:35:01 +0100

    [UPDATE] tests fields for get in users

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+class AdminCustomUser(admin.ModelAdmin):
+    list_display = ["email", "full_name", "is_active","is_staff","is_superuser"]
+
+    @admin.display(empty_value="???")
+    def view_birth_date(self, obj):
+        return obj.email
+
+
+admin.site.register(CustomUser, AdminCustomUser)
--- server/users/tests/API/test_get.py
+++ server/users/tests/API/test_get.py
@@ -1,5 +1,7 @@
+import pytest
 import requests
 from ..fixtures.user_fixtures import URL_ADMIN_LOGIN, BASE_URL
+from django.contrib.auth import get_user_model
 
 
 class TestGet:
@@ -9,6 +11,7 @@ class TestGet:
     def test_users_get_endpoint_is_list(self, users_response):
         assert isinstance(users_response['results'], list)
 
+    #@pytest.mark.django_db
     def test_fields_get_users_respons_structure(self, users_response):
         assert 'count' in users_response and isinstance(users_response['count'], int)
         assert 'next' in users_response
@@ -16,9 +19,9 @@ class TestGet:
         assert 'results' in users_response
         for user in users_response['results']:
             assert 'id' in user and isinstance(users_response['results'][0]['id'], int)
-            assert 'email' in user
-            assert 'full_name' in user
-            assert 'is_active' in user
-            assert 'is_staff' in user
-            assert 'is_superuser' in user
+            assert 'email' in user and user['email'].strip() != '' and isinstance(user['email'], str)
+            assert 'full_name' in user and isinstance(user['email'], str)
+            assert 'is_active' in user and isinstance(user['is_active'], bool)
+            assert 'is_staff' in user and isinstance(user['is_staff'], bool)
+            assert 'is_superuser' in user and isinstance(user['is_superuser'], bool)
 
