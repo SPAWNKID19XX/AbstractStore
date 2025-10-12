2460612fc5aa69d2a4a84e0808fd9921463e211f
boris-isac <borisisac@gmail.com>
2025-10-12 12:24:18 +0100

    [UPDATE] tests fields for get in users

--- server/users/tests/API/test_get.py
+++ server/users/tests/API/test_get.py
@@ -1,10 +1,24 @@
 import requests
 from ..fixtures.user_fixtures import URL_ADMIN_LOGIN, BASE_URL
 
+
 class TestGet:
     def test_users_get_endpoint_results_is_returns(self, users_response):
         assert 'results' in users_response, "result does not exist in returns data"
 
-
-    def test_users_get_endpoint_is_list(self,users_response):
+    def test_users_get_endpoint_is_list(self, users_response):
         assert isinstance(users_response['results'], list)
+
+    def test_fields_get_users_respons_structure(self, users_response):
+        assert 'count' in users_response and isinstance(users_response['count'], int)
+        assert 'next' in users_response
+        assert 'previous' in users_response
+        assert 'results' in users_response
+        for user in users_response['results']:
+            assert 'id' in user and isinstance(users_response['results'][0]['id'], int)
+            assert 'email' in user
+            assert 'full_name' in user
+            assert 'is_active' in user
+            assert 'is_staff' in user
+            assert 'is_superuser' in user
+
