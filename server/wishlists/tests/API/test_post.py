c956c5374bd6cea486566c8b94e11ad460b7b39d
boris-isac <borisisac@gmail.com>
2025-10-30 20:17:18 +0000

    [ADD] wishlistt starw tests

--- server/wishlists/tests/API/test_post.py
+++ server/wishlists/tests/API/test_post.py
@@ -1,6 +1,7 @@
 import pytest
 from django.db import connection
 from products.models import CustomProduct
+from pytest_django.fixtures import client
 from wishlists.models import Wishlist
 
 @pytest.mark.django_db
@@ -9,3 +10,16 @@ class TestWishlists:
         table = Wishlist._meta.db_table
         tables = set(connection.introspection.table_names())
         assert table in tables
+
+    def test_add_product_to_wishlist(self, auth_client ,product_data):
+        client, user = auth_client
+
+        print('««««???', user)
+        new_product = client.post(
+            "/products/api/v1/",
+            data=product_data,
+            format='json'
+        )
+
+        print('prod', new_product)
+
--- server/wishlists/tests/conftest.py
+++ server/wishlists/tests/conftest.py
@@ -1,2 +1,2 @@
-
+pytest_plugins = ("products.tests.conftest",)
 
